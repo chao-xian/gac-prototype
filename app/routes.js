7e886db34f44cc1265f80966e90e36074a995c82
chao-xian <gds@kelvingan.co.uk>
2018-10-05 13:49:54 +0100

    Switch to swatches and sort gallery results based on dominant colour

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-const artPieces = require(__dirname+"/data/art")
+const colours = require(__dirname+'/data/colours');
+const artPieces = require(__dirname+'/data/art');
 
 // Add your routes here - above the module.exports line
 
@@ -11,6 +12,13 @@ router.get("/", function(req, res) {
   const hashMatch = /\#/;
   let rgbVal = '';
 
+
+  // Make page have a bunch of links for each swatch
+  // Link sends off param with the rgb hex value
+  // When it gets here, we search the data for items that have that colour
+  // and then sort
+
+
   if (req.query.rgb !== undefined) {
     rgbVal = req.query.rgb.replace(encodedHashMatch, '').replace(hashMatch, '');
   }
@@ -19,13 +27,19 @@ router.get("/", function(req, res) {
 
   data.results = [];
 
-  for (let piece of artPieces.pieces) {
-    for (let pieceRgb of piece.rgb) {
-      if (data.searchRgb === pieceRgb) {
-        data.results.push(piece);
-        continue;
-      }
-    }
+  artPieces.sort(function(a, b){
+    return b[req.query.colour] - a[req.query.colour];
+  });
+
+  for (let piece of artPieces) {
+    console.log(`HI ${piece[req.query.colour]}`);
+    data.results.push(piece)
+    // for (let pieceRgb of piece.rgb) {
+      // if (data.searchRgb === pieceRgb) {
+      //   data.results.push(piece);
+      //   continue;
+      // }
+    // }
   }
 
   res.render('index', data);
