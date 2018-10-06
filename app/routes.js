0b79e590f88259c048b77b15c708f792770f08ea
Chao Xian <dev@kelvingan.co.uk>
2018-10-06 21:46:44 +0100

    Make logging say something useful

--- app/routes.js
+++ app/routes.js
@@ -26,7 +26,7 @@ router.get("/", function(req, res) {
     });
 
     for (let piece of artPieces) {
-      console.log(`HI ${piece[req.query.colour]}`);
+      console.log(`${data.colour} percentage: ${piece[data.colour]}`);
       data.results.push(piece);
     }
   }
